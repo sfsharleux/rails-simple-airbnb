9119c77925574ae0ac800bef7511bafc6b2a9801
sfsharleux <s.harleux@gmail.com>
2020-11-11 14:26:15 +0100

    spelling mistake

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do
+  name = Faker::BossaNova.song
+  address = Faker::Address.full_address
+  description = Faker::Quote.yoda
+  price_per_night = rand(1..500)
+  number_of_guests = rand(1..10)
+
+  Flat.create!(
+    name: name,
+    address: address,
+    description: description,
+    price_per_night: price_per_night,
+    number_of_guests: number_of_guests
+  )
+end
